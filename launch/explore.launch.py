413633939495a39d829d54b6de7ae9b4cde84876
Thomas Kenning <tkenning1@sheffield.ac.uk>
2026-04-29 16:32:35 +0100

    Added launch for mapping launch file

--- launch/explore.launch.py
+++ launch/explore.launch.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
+
+    launch_dir = os.path.join(
+        get_package_share_directory('ele434_team15_2026'),
+        'launch'
+    )
+
     return LaunchDescription([
-        Node(
-            package = 'ele434_team15_2026',
-            executable='',
-            name=''
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    launch_dir,
+                    'Mapping.launch.py'
+                )
+            )
         )
     ])
-
-# Use ros2 launch ele434_team15_2026 explore.launch.py
